177a2861b1cbc22f509740fb16073a46119acb62
anbehindY <yanminthwin@gmail.com>
2023-03-06 19:23:56 +0630

    Make menu button interactive

--- script.js
+++ script.js
@@ -1,2 +1,4 @@
 const hamburger = document.querySelector('.hamburger')
 hamburger.addEventListener('click', () => { hamburger.classList.toggle('active'); });
+const mobileNav = document.querySelector('.top-nav')
+hamburger.addEventListener('click', () => { mobileNav.classList.toggle('active'); });
