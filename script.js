21f51f2405ea357ac5a1365552521650eeb05004
anbehindY <yanminthwin@gmail.com>
2023-03-07 03:04:31 +0630

    Update linter files

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
-const hamburger = document.querySelector('.hamburger')
+const hamburger = document.querySelector('.hamburger');
+const mobileNav = document.querySelector('.top-nav'); 
+const navItems = document.querySelector('.nav-items');
+
 hamburger.addEventListener('click', () => { hamburger.classList.toggle('active'); });
-const mobileNav = document.querySelector('.top-nav')
 hamburger.addEventListener('click', () => { mobileNav.classList.toggle('active'); });
+
+for (let i = 0; i < navItems.length; i += 1) {
+  navItems[i].addEventListener('click', () => {
+  document.body.style.overflowY = 'scroll';
+//   hamburger.classList.remove('active');
+  mobileNav.classList.remove('active');
+  });};
