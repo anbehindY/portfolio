9c6619cf8d658655feafb2b933fce4d1e833062b
anbehindY <yanminthwin@gmail.com>
2023-03-09 01:53:23 +0630

    Add functions to the popup window

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 const hamburger = document.querySelector('.hamburger');
 const mobileNav = document.querySelector('.top-nav');
 const navItems = document.querySelectorAll('.nav-items');
@@ -127,7 +128,7 @@ const cards = () => {
                           </ul>
                         </div>
                       </div>
-                      <button type="submit" class="project-button">See Project</button>
+                      <button onclick='openModal()' type="submit" class="project-button">See Project</button>
                     </div> `,
   );
   return card;
@@ -151,10 +152,54 @@ const firstCard = ` <div id="yoga-img"></div>
                           <li>${cardsInfo[0].tech_stack.tech3}</li>
                         </ul>
                       </div>
-                      <button id="project-button" type="submit">See Project</button>
+                      <button onclick='openModal()' id="project-button" type="submit" href='#modal'>See Project</button>
                     </div>`;
+
+const modalMobile = `<div id="modal "class="modal">
+                        <header class='modal-header'>
+                          <h1 class="modal-heading">Multi-Post Stories</h1>
+                          <div class="close-btn" onclick="closeModal()">
+                            <div class="rotator1"></div>
+                            <div class="rotator2"></div>
+                          </div>
+                        </header>
+                        <div class="tech-box popup">
+                          <ul>
+                            <li>html</li>
+                            <li>Bootstrap</li>
+                            <li>Ruby on rails</li>
+                          </ul>
+                        </div>
+                        <div class="content">
+                          <div id="img10"></div>
+                          <div class="right-section">
+                            <p class="description popup">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent</p>
+                            <div class="button-container">
+                              <button type="submit" id="seeLive" class="project-button popup">See Live<img src="icons/seeLive.svg" /></button>
+                              <button type="submit" id="seeSource" class="project-button popup">See Source<img src="icons/seeSource.svg" /></button>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                      <div class="overlay"></div>`;
 
 window.addEventListener('load', () => {
   document.getElementById('first-project').innerHTML = firstCard;
   document.querySelector('.card-container').innerHTML = cards().join('');
 });
+
+const openModal = () => {
+  document.getElementById('modal-container').innerHTML = modalMobile;
+  const modal = document.querySelector('.modal');
+  const overlay = document.querySelector('.overlay');
+  modal.classList.add('active');
+  overlay.classList.add('active');
+};
+
+function closeModal() {
+  const modal = document.querySelector('.modal');
+  const overlay = document.querySelector('.overlay');
+  modal.classList.remove('active');
+  overlay.classList.remove('active');
+  document.getElementById('modal-container').removeChild(document.querySelector('.modal'));
+}
