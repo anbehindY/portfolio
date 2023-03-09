cce9b62cd4fac2e18c40515295b58c55031ae0a3
anbehindY <yanminthwin@gmail.com>
2023-03-10 02:18:35 +0630

    Modify funtionalities to display information

--- script.js
+++ script.js
@@ -42,7 +42,7 @@ const cardsInfo = [
     id: 2,
     class: 'healthCare',
     image: '#',
-    title: 'Profesional Art Printing Data',
+    title: 'Data Dashboard Healthcare',
     description: 'A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry\'s standard',
     tech_stack: {
       tech1: 'html',
@@ -56,7 +56,7 @@ const cardsInfo = [
     id: 3,
     class: 'website',
     image: '#',
-    title: 'Profesional Art Printing Data',
+    title: 'Website Portfollio',
     description: 'A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry\'s standard',
     tech_stack: {
       tech1: 'html',
@@ -84,7 +84,7 @@ const cardsInfo = [
     id: 5,
     class: 'healthCare',
     image: '#',
-    title: 'Profesional Art Printing Data',
+    title: 'Data Dashboard Healthcare',
     description: 'A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry\'s standard',
     tech_stack: {
       tech1: 'html',
@@ -98,7 +98,7 @@ const cardsInfo = [
     id: 6,
     class: 'website',
     image: '#',
-    title: 'Profesional Art Printing Data',
+    title: 'Website Portfollio',
     description: 'A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry\'s standard',
     tech_stack: {
       tech1: 'html',
@@ -112,52 +112,70 @@ const cardsInfo = [
 
 const cards = () => {
   const card = cardsInfo.map(
-    (cardInfo) => ` <div class="card ${cardInfo.class}">
+    (cardsInfo) => ` <div class="card ${cardsInfo.class}">
                       <div class="description">
-                        <div class="heading">${cardInfo.title}</div>
+                        <div class="heading">${cardsInfo.title}</div>
                         <div class="support-text">
                           <p>
-                            ${cardInfo.description}
+                            ${cardsInfo.description}
                           </p>
                         </div>
                         <div class="tech-stack">
                           <ul>
-                            <li>${cardInfo.tech_stack.tech1}</li>
-                            <li>${cardInfo.tech_stack.tech2}</li>
-                            <li>${cardInfo.tech_stack.tech3}</li>
+                            <li>${cardsInfo.tech_stack.tech1}</li>
+                            <li>${cardsInfo.tech_stack.tech2}</li>
+                            <li>${cardsInfo.tech_stack.tech3}</li>
                           </ul>
                         </div>
                       </div>
-                      <button onclick='openModal()' type="submit" class="project-button">See Project</button>
+                      <button onclick='popupInfo(${cardsInfo.id})' type="submit" class="project-button">See Project</button>
                     </div> `,
   );
   return card;
 };
 
+const firstCardInfo = {
+  id: 1,
+  class: 'more',
+  image: '#',
+  title2: 'Multi-Post Stories',
+  details: 'A daily selection of privately personalized reads; no accounts or sign-ups required. This has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a standard dummy text.',
+  tech_stack: {
+    tech0: 'css',
+    tech1: 'html',
+    tech2: 'bootstrap',
+    tech3: 'Ruby',
+  },
+};
+
 const firstCard = ` <div id="yoga-img"></div>
                     <div id="card">
                       <div id="main-text">
-                        <h3>${cardsInfo[0].title2}</h3>
+                        <h3>${firstCardInfo.title2}</h3>
                       </div>
                       <div id="support-text">
                         <p>
-                          ${cardsInfo[0].description1}
+                          ${firstCardInfo.details}
                         </p>
                       </div>
                       <div class="tech-box">
                         <ul>
-                          <li>${cardsInfo[0].tech_stack.tech0}</li>
-                          <li>${cardsInfo[0].tech_stack.tech1}</li>
-                          <li>${cardsInfo[0].tech_stack.tech2}</li>
-                          <li>${cardsInfo[0].tech_stack.tech3}</li>
+                          <li>${firstCardInfo.tech_stack.tech0}</li>
+                          <li>${firstCardInfo.tech_stack.tech1}</li>
+                          <li>${firstCardInfo.tech_stack.tech2}</li>
+                          <li>${firstCardInfo.tech_stack.tech3}</li>
                         </ul>
                       </div>
                       <button onclick='openfirst()' id="project-button" type="submit" href='#modal'>See Project</button>
                     </div>`;
 
-const modalMobile = ` <div id="modal "class="modal">
+const popupInfo = (projectId) => {
+  if (projectId == null) return;
+  let checked = cardsInfo.filter((elem) => elem.id === projectId);
+  checked = checked.shift();
+  const modalMobile = ` <div id="modal "class="modal">
                         <header class='modal-header'>
-                          <h1 class="modal-heading font-size">${cardsInfo[1].title}</h1>
+                          <h1 class="modal-heading font-size">${checked.title}</h1>
                           <div class="close-btn" onclick="closeModal()">
                             <div class="rotator1"></div>
                             <div class="rotator2"></div>
@@ -165,15 +183,15 @@ const modalMobile = ` <div id="modal "class="modal">
                         </header>
                         <div class="tech-box popup">
                           <ul>
-                            <li>${cardsInfo[1].tech_stack.tech1}</li>
-                            <li>${cardsInfo[1].tech_stack.tech2}</li>
-                            <li>${cardsInfo[1].tech_stack.tech3}</li>
+                            <li>${checked.tech_stack.tech1}</li>
+                            <li>${checked.tech_stack.tech2}</li>
+                            <li>${checked.tech_stack.tech3}</li>
                           </ul>
                         </div>
                         <div class="content">
                           <div id="img10"></div>
                           <div class="right-section">
-                            <p class="description popup">${cardsInfo[0].details}</p>
+                            <p class="description popup">${checked.description}</p>
                             <div class="button-container">
                               <button type="submit" id="seeLive" class="project-button popup">See Live<img src="icons/seeLive.svg" /></button>
                               <button type="submit" id="seeSource" class="project-button popup">See Source<img src="icons/seeSource.svg" /></button>
@@ -182,10 +200,16 @@ const modalMobile = ` <div id="modal "class="modal">
                         </div>
                       </div>
                       <div class="overlay"></div>`;
+  document.getElementById('modal-container').innerHTML = modalMobile;
+  const modal = document.querySelector('.modal');
+  const overlay = document.querySelector('.overlay');
+  modal.classList.add('active');
+  overlay.classList.add('active');
+};
 
 const modalfirst = `<div id="modal "class="modal">
                       <header class='modal-header'>
-                        <h1 class="modal-heading">${cardsInfo[0].title2}</h1>
+                        <h1 class="modal-heading">${firstCardInfo.title2}</h1>
                         <div class="close-btn" onclick="closeModal()">
                           <div class="rotator1"></div>
                           <div class="rotator2"></div>
@@ -193,16 +217,16 @@ const modalfirst = `<div id="modal "class="modal">
                       </header>
                       <div class="tech-box popup">
                         <ul>
-                          <li>${cardsInfo[0].tech_stack.tech0}</li>
-                          <li>${cardsInfo[0].tech_stack.tech1}</li>
-                          <li>${cardsInfo[0].tech_stack.tech2}</li>
-                          <li>${cardsInfo[0].tech_stack.tech3}</li>
+                          <li>${firstCardInfo.tech_stack.tech0}</li>
+                          <li>${firstCardInfo.tech_stack.tech1}</li>
+                          <li>${firstCardInfo.tech_stack.tech2}</li>
+                          <li>${firstCardInfo.tech_stack.tech3}</li>
                         </ul>
                       </div>
                       <div class="content">
                         <div id="img10"></div>
                         <div class="right-section">
-                          <p class="description popup">${cardsInfo[0].details}</>
+                          <p class="description popup">${firstCardInfo.details}</>
                           <div class="button-container">
                             <button type="submit" id="seeLive" class="project-button popup">See Live<img src="icons/seeLive.svg" /></button>
                             <button type="submit" id="seeSource" class="project-button popup">See Source<img src="icons/seeSource.svg" /></button>
@@ -216,14 +240,6 @@ window.addEventListener('load', () => {
   document.getElementById('first-project').innerHTML = firstCard;
   document.querySelector('.card-container').innerHTML = cards().join('');
 });
-
-const openModal = () => {
-  document.getElementById('modal-container').innerHTML = modalMobile;
-  const modal = document.querySelector('.modal');
-  const overlay = document.querySelector('.overlay');
-  modal.classList.add('active');
-  overlay.classList.add('active');
-};
 
 const openfirst = () => {
   document.getElementById('modal-container').innerHTML = modalfirst;
